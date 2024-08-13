ce42223d43dab4b9540d328e575abfab78b05a9e
Joshua Raphael <josh.n.raphael@gmail.com>
2024-08-13 05:46:54 -0700

    add remaining unit tests for http responses

--- http/response_test.go
+++ http/response_test.go
@@ -130,3 +130,111 @@ func TestResponseObject(tt *testing.T) {
 		})
 	}
 }
+
+func TestResponseList(tt *testing.T) {
+	tests := []struct {
+		name        string
+		code        int
+		listBody    []testObj
+		errBody     models.ErrorResponse
+		readerInput func(inputBytes []byte, errorBytes []byte) string
+		assert      func(t *testing.T, list []testObj, err error)
+	}{
+		{
+			name: "fail to decode response",
+			code: http.StatusOK,
+			readerInput: func(inputBytes []byte, errorBytes []byte) string {
+				return "?"
+			},
+			assert: func(t *testing.T, list []testObj, err error) {
+				require.Nil(t, list)
+				require.EqualError(t, err, "invalid character '?' looking for beginning of value")
+			},
+		},
+		{
+			name: "success",
+			code: http.StatusOK,
+			listBody: []testObj{
+				{
+					ID:   "8710298370",
+					Name: "test",
+				},
+				{
+					ID:   "1212121",
+					Name: "newName",
+				},
+			},
+			readerInput: func(inputBytes []byte, errorBytes []byte) string {
+				return string(inputBytes)
+			},
+			assert: func(t *testing.T, list []testObj, err error) {
+				require.Len(t, list, 2)
+				require.Equal(t, "8710298370", list[0].ID)
+				require.Equal(t, "test", list[0].Name)
+				require.Equal(t, "1212121", list[1].ID)
+				require.Equal(t, "newName", list[1].Name)
+				require.NoError(t, err)
+			},
+		},
+		{
+			name: "not authorized - error marshalling response",
+			code: http.StatusUnauthorized,
+			readerInput: func(inputBytes []byte, errorBytes []byte) string {
+				return "?"
+			},
+			assert: func(t *testing.T, list []testObj, err error) {
+				require.Nil(t, list)
+				require.EqualError(t, err, "invalid character '?' looking for beginning of value")
+			},
+		},
+		{
+			name: "not authorized",
+			code: http.StatusUnauthorized,
+			errBody: models.ErrorResponse{
+				Message: "test",
+				Errors: []models.ErrorDetail{
+					{
+						Status: http.StatusUnauthorized,
+						Code:   "unauthorized",
+						Title:  "Not Authorized",
+					},
+				},
+			},
+			readerInput: func(inputBytes []byte, errorBytes []byte) string {
+				return string(errorBytes)
+			},
+			assert: func(t *testing.T, list []testObj, err error) {
+				require.Nil(t, list)
+				require.EqualError(t, err, "error responses: [401] Not Authorized")
+			},
+		},
+		{
+			name: "unknown error",
+			code: http.StatusInternalServerError,
+			readerInput: func(inputBytes []byte, errorBytes []byte) string {
+				return string(errorBytes)
+			},
+			assert: func(t *testing.T, list []testObj, err error) {
+				require.Nil(t, list)
+				require.EqualError(t, err, "unknown error returned: 500")
+			},
+		},
+	}
+	for i := range tests {
+		test := tests[i]
+		tt.Run(test.name, func(t *testing.T) {
+			listBytes, err := json.Marshal(test.listBody)
+			require.NoError(t, err)
+			errBytes, err := json.Marshal(test.errBody)
+			require.NoError(t, err)
+			reader := bytes.NewBufferString(test.readerInput(listBytes, errBytes))
+			readerCloser := io.NopCloser(reader)
+			r := &http.Response{
+				StatusCode: test.code,
+				Body:       readerCloser,
+			}
+			list, err := raHttp.ResponseList[testObj](r)
+			test.assert(t, list, err)
+		})
+	}
+}
